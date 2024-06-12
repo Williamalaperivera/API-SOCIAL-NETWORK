be4d8607bf9281684d61c1ce2b4af2877281be13
William <williamjoel273@gmail.com>
2024-06-12 18:37:36 -0500

    create new folder, connection.jsx and index.js, insert the code

---
+++ index.js
@@ -0,0 +1,38 @@
+// Importaciones
+const connection = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
+
+// Mensaje de bienvenida
+console.log("API NODE arriba");
+
+// Conexión a la BD
+connection();
+
+
+// Crear servidor de Node
+const app = express();
+const puerto = 3900;
+
+// Configurar cors: permite que las peticiones se hagan correctamente
+app.use(cors());
+
+// Conversión de datos (body a objetos JS)
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Configurar rutas
+app.get('/test-route', (req, res)=> {
+  return res.status(200).json(
+    {
+      'id': 1,
+      'name': 'Inés María',
+      'username': 'inesmaoria'
+    }
+  );
+})
+
+// Configurar el servidor para escuchar las peticiones HTTP
+app.listen(puerto, () => {
+  console.log("Servidor de NODE corriendo en el puerto", puerto)
+});
---
+++ start-project.bat
@@ -0,0 +1,2 @@
+start cmd.exe /k "cd /d C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath C:\baseDatos"
+start cmd.exe /k "cd /d C:\Users\willi\OneDrive\Desktop\api-social-network\api-social-network && npm run start:watch"
